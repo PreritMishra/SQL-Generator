70f006b23d7a7f9e67863b891fb5113b030ef7ab
Prerit <preritmishra1002@gmail.com>
2023-05-15 15:51:13 +0530

    called api from frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,26 @@ import { useState } from 'react';
 
 function App() {
   const [queryDescription, setQueryDescription] = useState("");
-  const onSubmit = (e) => {
+  const [sqlQuery, setSqlQuery] = useState("");
+
+
+  const onSubmit = async (e) => {
     e.preventDefault();
-    console.log("form submitted", queryDescription);
+    const generatedQuery = await generateQuery();
+    setSqlQuery(generatedQuery);
   }
+
+  const generateQuery = async () => {
+    const response = await fetch(" http://localhost:3005/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({queryDescription: queryDescription}),
+    }); 
+    const data = await response.json();
+    return data.response.trim();
+  };
 
   return (
     <main className= {styles.main}>
@@ -23,6 +39,7 @@ function App() {
           onChange={(e) => setQueryDescription(e.target.value)}
         />
         <input type="submit" value="Generate query" />
+        <pre>{sqlQuery}</pre>
       </form>
     </main>
   )
